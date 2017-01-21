2650c42643e2f34fd3d92c9aa40ab211f9734089
Alex Jordan <alex@strugee.net>
2017-01-20 23:39:00 -0500

    Move templates to an app-wide location

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'gettingstarted.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'debug': True,
